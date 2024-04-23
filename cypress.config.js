9bd0a7cc25d779386324c8ff2caca28f949d38a9
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-23 15:18:42 -0300

    adiciona app

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://rarolabs.com.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
